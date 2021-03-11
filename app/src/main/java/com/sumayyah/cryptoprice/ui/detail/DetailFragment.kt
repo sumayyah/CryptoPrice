b799bac988ee59d376588d7e4f4ff8695d299ee8
Sumayyah Ahmed <Sumayyah_Ahmed@Comcast.com>
2021-03-11 10:33:19 -0500

    Add a detail fragment

---
+++ app/src/main/java/com/sumayyah/cryptoprice/ui/detail/DetailFragment.kt
@@ -0,0 +1,50 @@
+package com.sumayyah.cryptoprice.ui.detail
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.sumayyah.cryptoprice.MainApplication
+import com.sumayyah.cryptoprice.R
+import com.sumayyah.cryptoprice.ui.main.MainViewModel
+import com.sumayyah.cryptoprice.ui.main.MainViewModelFactory
+import javax.inject.Inject
+
+class DetailFragment : Fragment() {
+
+    private lateinit var label: TextView
+    private lateinit var name: TextView
+    private lateinit var price: TextView
+    private lateinit var volume: TextView
+    private lateinit var timestamp: TextView
+
+    @Inject
+    lateinit var viewModelFactory: MainViewModelFactory
+
+    val viewModel: MainViewModel by activityViewModels { viewModelFactory }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity?.application as MainApplication).component.inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+
+        label = view.findViewById(R.id.label_value)
+        name = view.findViewById(R.id.name_value)
+        price = view.findViewById(R.id.price_value)
+        volume = view.findViewById(R.id.volume_value)
+        timestamp = view.findViewById(R.id.timestamp_value)
+
+        return view
+    }
+}
