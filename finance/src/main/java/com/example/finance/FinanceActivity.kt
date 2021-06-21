7e4d54be04cc8af24c4fa284e8b1aa300018c2b3
Sumayyah Ahmed <sumayyah16.m@gmail.com>
2021-06-21 10:51:29 -0400

    Add basic finance activity

---
+++ finance/src/main/java/com/example/finance/FinanceActivity.kt
@@ -0,0 +1,13 @@
+package com.example.finance
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FinanceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_finance)
+    }
+}
